98c8a503a36ed850f0752586531e0eab00f2540a
JohnJones362 <97484997+JohnJones362@users.noreply.github.com>
2022-02-02 09:47:19 -0600

    Update Jones_13122.cpp

--- Jones_13122.cpp
+++ Jones_13122.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main()
 {
+  int matching_cards [3][3]={{1,2,3},{2,3,1},{3,1,2}};
   for (int i=0; i<n ;i++)
-  {
-  }
   for (int j=0; j<m; j++)
   {
+    cout << "Enter a value";
+    cin >> array[i][j];
   }
   if (i == j)
     cout << "Congratulations! You found a match! Do you want to play again or exit?" << endl;
